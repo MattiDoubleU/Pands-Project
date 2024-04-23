f0a557357404a81f67e10da1c4cef64adb579664
MattiDoubleU <luciernagas@gmx.de>
2024-04-23 23:13:26 +0100

    Getting started

---
+++ analysis.py
@@ -0,0 +1,12 @@
+# Data frames.
+import pandas as pd
+
+# Plotting.
+import matplotlib.pyplot as plt
+
+# Numerical arrays.
+import numpy as np
+
+### Load Data
+# Fetch data from URL.
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
