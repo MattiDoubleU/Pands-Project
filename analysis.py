b9bf69ff39d59440723179be04d4c4f793060cfe
MattiDoubleU <luciernagas@gmx.de>
2024-05-08 22:59:32 +0100

    Histograms of all four variables completed

--- analysis.py
+++ analysis.py
@@ -57,13 +57,12 @@ slen = df['sepal_length'].to_numpy()
 # Sepal width.
 swidth = df['sepal_width'].to_numpy()
 
-# Create histogram.
-
+# Create histogram for a) petal width for each of the three species
 plt.hist([df[df['species'] == 'setosa']['petal_width'],
           df[df['species'] == 'versicolor']['petal_width'],
           df[df['species'] == 'virginica']['petal_width']],
-         bins=10, stacked=False, color=['green', 'blue', 'orange'],
-         alpha=0.8, label=['setosa', 'versicolor', 'virginica'])
+         bins='auto', stacked=False, color=['green', 'blue', 'orange'],
+         alpha=0.6, label=['setosa', 'versicolor', 'virginica']) # The alpha parameter in Matplotlib adjusts the transparency of the plotted elements.
 
 # Set labels and title
 plt.xlabel('Petal width (cm)')
@@ -73,12 +72,93 @@ plt.title('Petal width for Setosa, Versicolor & Virginica species')
 # Add legend.
 plt.legend()
 
+# Save to png
+plt.savefig('petal_width_histogram.png')
+
 # Show plot.
 plt.show()
+
+# Repeating above code for b) petal length
+plt.hist([df[df['species'] == 'setosa']['petal_length'],
+          df[df['species'] == 'versicolor']['petal_length'],
+          df[df['species'] == 'virginica']['petal_length']],
+         bins='auto', stacked=False, color=['green', 'blue', 'orange'],
+         alpha=0.6, label=['setosa', 'versicolor', 'virginica'])
+
+# Set labels and title
+plt.xlabel('Petal length (cm)')
+plt.ylabel('Count')
+plt.title('Petal length for Setosa, Versicolor & Virginica species')
+
+# Add legend
+plt.legend()
+
+# Save to png
+plt.savefig('petal_length_histogram.png')
+
+# Show plot
+plt.show()
+
+# c) Sepal width
+plt.hist([df[df['species'] == 'setosa']['sepal_width'],
+          df[df['species'] == 'versicolor']['sepal_width'],
+          df[df['species'] == 'virginica']['sepal_width']],
+         bins='auto', stacked=False, color=['green', 'blue', 'orange'],
+         alpha=0.6, label=['setosa', 'versicolor', 'virginica'])
+
+# Set labels and title
+plt.xlabel('Sepal width (cm)')
+plt.ylabel('Count')
+plt.title('Sepal width for Setosa, Versicolor & Virginica species')
+
+# Add legend
+plt.legend()
+
+# Save to png
+plt.savefig('sepal_width_histogram.png')
+
+# Show plot
+plt.show()
+
+
+# d) Sepal length
+plt.hist([df[df['species'] == 'setosa']['sepal_length'],
+          df[df['species'] == 'versicolor']['sepal_length'],
+          df[df['species'] == 'virginica']['sepal_length']],
+         bins='auto', stacked=False, color=['green', 'blue', 'orange'],
+         alpha=0.6, label=['setosa', 'versicolor', 'virginica'])
+
+# Set labels and title
+plt.xlabel('Sepal length (cm)')
+plt.ylabel('Count')
+plt.title('Sepal length for Setosa, Versicolor & Virginica species')
+
+# Add legend
+plt.legend()
+
+# Save to png
+plt.savefig('sepal_length_histogram.png')
+
+# Show plot
+plt.show()
+
+
+
+
+
 
 # Below here is just scrippling for now.
 
 '''
+from plotly.express import scatter_3d
+# Plotting in 3D by plotly.express that would show the plot with capability of zooming,
+# changing the orientation, and rotating
+scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_length', size="petal_width",
+                   color="species", color_discrete_map={"Joly": "blue", "Bergeron": "violet", "Coderre": "pink"})\
+            .show()
+
+
+
 
 # Create simple Plot.
 plt.plot(plen, pwidth, 'o')   # Displays as 'Os'
